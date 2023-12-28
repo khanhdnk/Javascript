b1d18b0dfbf774539008070ec9c900dc3e026e5a
khanhdnk <dangnamkhanhkdgv@gmail.com>
2023-12-28 11:27:05 +0700

    update

--- Typescript/index.js
+++
@@ -1,6 +0,0 @@
-// `keyof StringMap` resolves to `string` here
-function createStringPair(property, value) {
-    var _a;
-    return _a = {}, _a[property] = value, _a;
-}
-console.log(createStringPair('name', 'ok'));
