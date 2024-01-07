8735d333306fadad1b6909432efc5214d46be9b0
khanhdnk <121726197+khanhdnk@users.noreply.github.com>
2024-01-07 10:32:12 +0700

    update

---
+++ nextapp/src/app/hello/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Home(){
+    return (
+        <Link href={'/youtube'}>
+            <h1>Facebook</h1>
+        
+        </Link>
+    );
+}
